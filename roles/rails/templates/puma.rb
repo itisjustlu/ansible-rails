8976fc6c4655e0224b3ac5c611d7f867049b408a
Lucas Juarez <lucasjavierjuarez@hotmail.com>
2017-12-06 12:10:54 -0300

    Some minor changes

--- roles/rails/templates/puma.rb
+++ roles/rails/templates/puma.rb
@@ -29,3 +29,53 @@ end
 before_fork do
   ActiveRecord::Base.connection_pool.disconnect!
 end
+
+
+
+
+
+
+
+
+
+#!/usr/bin/env puma
+
+directory '{{ deploy_dir  }}{{ deploy_app_name }}/current'
+rackup "{{ deploy_dir  }}{{ deploy_app_name }}/current/config.ru"
+environment '{{ environment }}'
+
+tag ''
+
+pidfile "{{ deploy_dir  }}{{ deploy_app_name }}/shared/tmp/pids/puma.pid"
+state_path "{{ deploy_dir  }}{{ deploy_app_name }}/shared/tmp/pids/puma.state"
+stdout_redirect '{{ deploy_dir  }}{{ deploy_app_name }}/current/log/puma.error.log', '{{ deploy_dir  }}{{ deploy_app_name }}/current/log/puma.access.log', true
+
+
+threads {{ puma_threads }}
+
+
+
+bind 'unix://{{ deploy_dir  }}{{ deploy_app_name }}/shared/tmp/sockets/stackprint-puma.sock'
+
+workers {{ puma_workers }}
+
+preload_app!
+
+
+on_restart do
+  puts 'Refreshing Gemfile'
+  ENV["BUNDLE_GEMFILE"] = "{{ deploy_dir  }}{{ deploy_app_name }}/current/Gemfile"
+end
+
+
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect!
+end
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
+
+
